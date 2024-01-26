8a58c88f57ee9a25960b136e257741fa5571b388
sangwook.yoo <sangwook.yoo@cj.net>
2024-01-26 09:48:09 +0900

    Add Swagger api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoardModule } from './board/board.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), BoardModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), BoardModule, AuthModule],
   controllers: [],
   providers: [],
 })
